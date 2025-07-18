11cae47bc6d25db6a19ab702e1cf8836e9d241a9
Daniel O <dbokely@gmail.com>
2025-07-18 14:22:14 +0800

    Added sample files

--- SwimNormaliser/Program.cs
+++ SwimNormaliser/Program.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using Math.Gps;
+using Math.Tools.TrackReaders;
+
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
@@ -6,6 +8,11 @@ var service = new SwimNormaliser.Services.TcxFileReader("example.tcx");
 
 Console.WriteLine(service.GetType().Name);
 
+
+
 Console.ReadLine();
+
+var x = Math.Tools.TrackReaders.Deserializer.File("");
+
 
 return;
