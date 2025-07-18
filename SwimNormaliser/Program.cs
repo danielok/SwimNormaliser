6e3f12107a9833db7d78cd3d2e12d9b68951e754
Daniel O <dbokely@gmail.com>
2025-07-18 14:42:14 +0800

    implement file reading functionality

--- SwimNormaliser/Program.cs
+++ SwimNormaliser/Program.cs
@@ -1,18 +1,28 @@
-﻿using Math.Gps;
+﻿using System.Runtime.Serialization;
+using Math.Gps;
 using Math.Tools.TrackReaders;
+using SwimNormaliser.Services;
 
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-var service = new SwimNormaliser.Services.TcxFileReader("example.tcx");
-
-Console.WriteLine(service.GetType().Name);
+Console.WriteLine("Swim File Normaliser");
 
 
+var sampleFilesDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Sample Files");
+if (!Directory.Exists(sampleFilesDirectory))
+{
+    Console.WriteLine($"Sample Files directory does not exist: {sampleFilesDirectory}");
+    return;
+}
+else
+{
+    
+    var firstActivityIds = TcxFileReader.GetFirstActivityIds(sampleFilesDirectory);
+    foreach (var kvp in firstActivityIds)
+    {
+        Console.WriteLine($"File: {kvp.Key}, First Activity ID: {kvp.Value}");
+    }
+}
 
+Console.WriteLine("Press any key to exit...");
 Console.ReadLine();
-
-var x = Math.Tools.TrackReaders.Deserializer.File("");
-
-
-return;
+Console.WriteLine("Exiting...");
--- SwimNormaliser/Services/TcxFileReader.cs
+++ SwimNormaliser/Services/TcxFileReader.cs
@@ -1,3 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
 namespace SwimNormaliser.Services
 {
 
@@ -8,6 +14,36 @@ namespace SwimNormaliser.Services
 
         }
 
-        
+        /// <summary>
+        /// Opens each TCX file from the Sample Files directory and returns the value of the ID element
+        /// from the first Activity element in the Activities element.
+        /// </summary>
+        /// <param name="sampleFilesDirectory">The path to the Sample Files directory.</param>
+        /// <returns>A dictionary mapping file names to the first Activity ID value.</returns>
+        public static Dictionary<string, string?> GetFirstActivityIds(string sampleFilesDirectory)
+        {
+            var result = new Dictionary<string, string?>();
+            var tcxFiles = Directory.GetFiles(sampleFilesDirectory, "*.tcx");
+
+            XNamespace ns = "http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2";
+
+            foreach (var file in tcxFiles)
+            {
+                try
+                {
+                    var doc = XDocument.Load(file);
+                    var activities = doc.Root?.Element(ns + "Activities");
+                    var firstActivity = activities?.Element(ns + "Activity");
+                    var idElement = firstActivity?.Element(ns + "Id");
+                    result[Path.GetFileName(file)] = idElement?.Value;
+                }
+                catch (Exception ex)
+                {
+                    result[Path.GetFileName(file)] = $"Error: {ex.Message}";
+                }
+            }
+
+            return result;
+        }
     }
 }
